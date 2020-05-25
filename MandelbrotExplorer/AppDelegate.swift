cfed8c4854d5e9eeeac66a3e9eb9d3a76fea9a7f
Jae-Seung Lee <jaeseung@gmail.com>
2020-05-25 11:07:22 -0400

    Initial Commit

---
+++ MandelbrotExplorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MandelbrotExplorer
+//
+//  Created by Jae Seung Lee on 5/25/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MandelbrotExplorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MandelbrotExplorer
+//
+//  Created by Jae Seung Lee on 5/25/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
