84bc6f1b1cb0dfef652d14552860f0b93be52b30
Jae-Seung Lee <jaeseung@gmail.com>
2020-06-28 14:06:56 -0400

    Create MandelbrotID

---
+++ MandelbrotExplorer/MandelbrotID.swift
@@ -0,0 +1,15 @@
+//
+//  MandelbrotID.swift
+//  MandelbrotExplorer
+//
+//  Created by Jae Seung Lee on 6/28/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+enum MandelbrotID: Int {
+    case first = 0
+    case second
+    case third
+}
