9abeb841be5ec63dd75b4211cdd61ae85423279e
Jae-Seung Lee <jaeseung@gmail.com>
2020-07-03 14:33:07 -0400

    Update MandelbrotSetCPU
    Conform MandelbrotSet
    Add generateCGImage(lengthOfRow:)

--- MandelbrotExplorer/MandelbrotSetCPU.swift
+++ MandelbrotExplorer/MandelbrotSetCPU.swift
@@ -9,7 +9,12 @@
 import Foundation
 
 class MandelbrotSetCPU: MandelbrotSet {
-    var _zs: [Complex]
+    private let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+    private let bitmapInfo:CGBitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+    private let bitsPerComponent: Int = 8
+    private let bitsPerPixel: Int = 32
+    
+    private var _zs: [Complex]
     var zs: [Complex] {
         get {
             return _zs
@@ -19,7 +24,7 @@ class MandelbrotSetCPU: MandelbrotSet {
         }
     }
     
-    var _maxIter: Int
+    private var _maxIter: Int
     var maxIter: Int {
         get {
             return _maxIter
@@ -29,7 +34,7 @@ class MandelbrotSetCPU: MandelbrotSet {
         }
     }
     
-    var _values: [Int]
+    private var _values: [Int]
     var values: [Int] {
         get {
             return _values
@@ -39,22 +44,35 @@ class MandelbrotSetCPU: MandelbrotSet {
         }
     }
     
+    private var _cgImage: CGImage?
+    var cgImage: CGImage {
+        get {
+            return _cgImage!
+        }
+        set {
+            _cgImage = newValue;
+        }
+    }
+    
+    var imgBytes: [SIMD4<UInt8>]
+    
     init(inZs: [Complex], inMaxIter: Int) {
         _zs = inZs
         _maxIter = inMaxIter
-            
         _values = [Int](repeating: 0, count: _zs.count)
+        imgBytes = [SIMD4<UInt8>](repeating: SIMD4<UInt8>(x: 0, y: 0, z: 0, w: 255), count: _zs.count)
 
         calculate()
+        generateCGImage(lengthOfRow: Int(sqrt(Double(values.count))))
     }
     
-    func calculate() -> Void{
+    func calculate() -> Void {
         values = zs.map({ (z0) -> Int in
             mandelbrotFormula(z0: z0)
         })
     }
     
-    func mandelbrotFormula(z0: Complex) -> Int {
+    private func mandelbrotFormula(z0: Complex) -> Int {
         var z = z0
         var iter = 0;
         while (iter < maxIter)
@@ -69,7 +87,27 @@ class MandelbrotSetCPU: MandelbrotSet {
         return (maxIter == iter) ? 0 : Int((Float(iter) / Float(maxIter) * 255.0))
     }
     
-    func toData() -> Data {
-        return Data()
+    private func generateCGImage(lengthOfRow: Int) -> Void {
+        for x in 0..<lengthOfRow {
+            for y in 0..<lengthOfRow {
+                let value = values[y * lengthOfRow + x]
+                imgBytes[(lengthOfRow - y - 1) * lengthOfRow + x] = SIMD4<UInt8>(x: UInt8(0), y: UInt8(value), z: UInt8(0), w: UInt8(255))
+            }
+        }
+        
+        let imgData = NSData(bytes: &imgBytes, length: imgBytes.count * MemoryLayout<SIMD4<UInt8>>.size)
+        let providerRef = CGDataProvider(data: imgData)
+        
+        _cgImage = CGImage(width: lengthOfRow,
+                           height: lengthOfRow,
+                           bitsPerComponent: bitsPerComponent,
+                           bitsPerPixel: bitsPerPixel,
+                           bytesPerRow: lengthOfRow * MemoryLayout<SIMD4<UInt8>>.size,
+                           space: rgbColorSpace,
+                           bitmapInfo: bitmapInfo,
+                           provider: providerRef!,
+                           decode: nil,
+                           shouldInterpolate: true,
+                           intent: CGColorRenderingIntent.defaultIntent)
     }
 }
