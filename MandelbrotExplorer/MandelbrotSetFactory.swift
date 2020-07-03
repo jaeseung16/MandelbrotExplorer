6b71878cbf3dac3978acefe6d9e287bc19ac3723
Jae-Seung Lee <jaeseung@gmail.com>
2020-07-03 14:28:28 -0400

    Create MandelbrotSetFactory

---
+++ MandelbrotExplorer/MandelbrotSetFactory.swift
@@ -0,0 +1,28 @@
+//
+//  MandelbrotSetFactory.swift
+//  MandelbrotExplorer
+//
+//  Created by Jae Seung Lee on 7/3/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+import MetalKit
+
+class MandelbrotSetFactory {
+    static func createMandelbrotSet(inZs: [Complex], inMaxIter: Int) -> MandelbrotSet {
+        let mandelbrotSet: MandelbrotSet
+        
+        let device = MTLCreateSystemDefaultDevice()
+        if (device == nil) {
+            print("Using CPU")
+            mandelbrotSet = MandelbrotSetCPU(inZs: inZs, inMaxIter: 200)
+        } else {
+            print("Using GPU")
+            mandelbrotSet = MandelbrotSetGPU(inZs: inZs, inMaxIter: 200)
+        }
+        
+        return mandelbrotSet
+    }
+}
+
