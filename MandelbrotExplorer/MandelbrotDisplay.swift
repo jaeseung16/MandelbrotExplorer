dd2646912505b182c898d632512aa556f827119b
Jae-Seung Lee <jaeseung@gmail.com>
2020-07-03 14:29:39 -0400

    Update MandelbrotDisplay
    Initialize mandelbrotSet by using MandelbrotSetFactory
    Simplify setImage(for:)

--- MandelbrotExplorer/MandelbrotDisplay.swift
+++ MandelbrotExplorer/MandelbrotDisplay.swift
@@ -12,15 +12,10 @@ import Cocoa
 class MandelbrotDisplay {
     var child: MandelbrotDisplay?
     
-    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-    let bitmapInfo:CGBitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
-    let bitsPerComponent: Int = 8
-    let bitsPerPixel: Int = 32
-    
     var sideLength: Int
     
     var zs: [Complex]
-    var imgBytes: [PixelData]
+    var imgBytes: [SIMD4<UInt8>]
     
     var mandelbrotRect = ComplexRect(Complex(-2.1, -1.5), Complex(0.9, 1.5))
     var mandelbrotSet: MandelbrotSet?
@@ -32,7 +27,7 @@ class MandelbrotDisplay {
         self.sideLength = sideLength + 1
         let numberOfPixels = self.sideLength * self.sideLength
         
-        self.imgBytes = [PixelData](repeating: PixelData(a: 255, r: 0, g: 0, b: 0), count: numberOfPixels)
+        self.imgBytes = [SIMD4<UInt8>](repeating: SIMD4<UInt8>(x: 0, y: 0, z: 0, w: 255), count: numberOfPixels)
         self.zs = [Complex](repeating: Complex(), count: numberOfPixels)
     }
     
@@ -48,13 +43,14 @@ class MandelbrotDisplay {
         let displaySize = CGSize(width: sideLength - 1, height: sideLength - 1)
         for x in 0..<sideLength {
             for y in 0..<sideLength {
-                zs[(sideLength - y - 1) * sideLength + x] = viewCoordinatesToComplexCoordinates(x: Double(x), y: Double(y), displaySize: displaySize)
+                zs[y * sideLength + x] = viewCoordinatesToComplexCoordinates(x: Double(x), y: Double(y), displaySize: displaySize)
             }
         }
         
         let timeToPrepare = Date()
         
-        mandelbrotSet = MandelbrotSetGPU(inZs: zs, inMaxIter: 200)
+        mandelbrotSet = MandelbrotSetFactory.createMandelbrotSet(inZs: zs, inMaxIter: 200)
+        mandelbrotSet?.calculate()
         
         let timeToCalculate = Date()
         
@@ -69,7 +65,7 @@ class MandelbrotDisplay {
         print("MandelbrotDisplay.generateMandelbrotSet(): It took \(timeToSetImage.timeIntervalSince(timeToCalculate)) seconds")
     }
     
-    func viewCoordinatesToComplexCoordinates(x: Double, y: Double, displaySize: CGSize) -> Complex {
+    private func viewCoordinatesToComplexCoordinates(x: Double, y: Double, displaySize: CGSize) -> Complex {
         let minReal = mandelbrotRect.minReal
         let maxReal = mandelbrotRect.maxReal
         let minImaginary = mandelbrotRect.minImaginary
@@ -80,7 +76,7 @@ class MandelbrotDisplay {
         return Complex(r, i)
     }
     
-    func viewCoordinatesToComplexCoordinates(x: Double, y: Double, displaySize: CGSize, in complexRect: ComplexRect) -> Complex {
+    static func viewCoordinatesToComplexCoordinates(x: Double, y: Double, displaySize: CGSize, in complexRect: ComplexRect) -> Complex {
         let minReal = complexRect.minReal
         let maxReal = complexRect.maxReal
         let minImaginary = complexRect.minImaginary
@@ -101,37 +97,15 @@ class MandelbrotDisplay {
         
         child.mandelbrotRect = ComplexRect(tl, br)
         child.generateMandelbrotSet()
-        
     }
     
-    func setImage(for mandelbrotSet: MandelbrotSet?) -> Void {
+    private func setImage(for mandelbrotSet: MandelbrotSet?) -> Void {
         guard let mandelbrotSet = self.mandelbrotSet else {
             print("self.mandelbrotSet is null")
             return
         }
         
-        imgBytes = mandelbrotSet.values.map { (value) -> PixelData in
-            PixelData(a: UInt8(255), r: UInt8(0), g: UInt8(value), b: UInt8(0))
-        }
-        
-        let imgData = NSData(bytes: &imgBytes, length: imgBytes.count * MemoryLayout<PixelData>.size)
-        let providerRef = CGDataProvider(data: imgData)
-        
-        let cgim = CGImage(
-            width: sideLength,
-            height: sideLength ,
-            bitsPerComponent: bitsPerComponent,
-            bitsPerPixel: bitsPerPixel,
-            bytesPerRow: sideLength * MemoryLayout<PixelData>.size,
-            space: rgbColorSpace,
-            bitmapInfo: bitmapInfo,
-            provider: providerRef!,
-            decode: nil,
-            shouldInterpolate: true,
-            intent: CGColorRenderingIntent.defaultIntent
-        )
-        
-        self.mandelbrotImage = NSImage(cgImage: cgim!, size: NSSize.zero)
+        self.mandelbrotImage = NSImage(cgImage: mandelbrotSet.cgImage, size: NSSize.zero)
     }
     
 }
