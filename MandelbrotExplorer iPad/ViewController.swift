dc59f3ec85ccb1699945f0f7536ac645813d7f14
Jae-Seung Lee <jaeseung@gmail.com>
2020-10-04 17:26:06 -0400

    Create iPad target

---
+++ MandelbrotExplorer iPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MandelbrotExplorer iPad
+//
+//  Created by Jae Seung Lee on 10/4/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
