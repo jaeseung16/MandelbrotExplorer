7c3aa246648e0eabb7a04073f6a70d5970acd119
Jae-Seung Lee <jaeseung@gmail.com>
2022-08-22 18:45:24 -0400

    Refactor: Deduplicate view modifiers

--- MandelbrotExplorer iPad/SwiftUIView/ZoomedMandelbrotView.swift
+++ MandelbrotExplorer iPad/SwiftUIView/ZoomedMandelbrotView.swift
@@ -16,30 +16,26 @@ struct ZoomedMandelbrotView: View {
         GeometryReader { geometry in
             let length = geometry.size.width < geometry.size.height ? geometry.size.width : geometry.size.height
             
-            if let image = viewModel.mandelbrotImage {
-                Image(uiImage: image)
-                    .frame(width: length, height: length)
-                    .onAppear {
-                        viewModel.generateMandelbrotSet(calculationSize: length)
-                    }
-                    .onChange(of: length) { _ in
-                        viewModel.generateMandelbrotSet(calculationSize: length)
-                    }
-            } else {
-                Rectangle()
-                    .strokeBorder(Color.green, lineWidth: 2.0)
-                    .frame(width: length, height: length)
-                    .onAppear {
-                        viewModel.generateMandelbrotSet(calculationSize: length)
-                    }
-                    .onChange(of: length) { _ in
-                        viewModel.generateMandelbrotSet(calculationSize: length)
-                    }
-            }
-                
-                
-            
-                
+            image
+                .frame(width: length, height: length)
+                .onAppear {
+                    viewModel.generateMandelbrotSet(calculationSize: length)
+                }
+                .onChange(of: length) { _ in
+                    viewModel.generateMandelbrotSet(calculationSize: length)
+                }
+                .onChange(of: viewModel.mandelbrotRect) { newValue in
+                    viewModel.generateMandelbrotSet(calculationSize: length)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private var image: some View {
+        if let uiImage = viewModel.mandelbrotImage {
+            Image(uiImage: uiImage)
+        } else {
+            Rectangle()
         }
     }
 }
