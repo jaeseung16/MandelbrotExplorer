d85bfbf49b4cca71175e301da2f7d6110d92ddb8
Jae-Seung Lee <jaeseung@gmail.com>
2022-08-28 14:26:20 -0400

    Refactor: Validate location and length

--- MandelbrotExplorer iPad/SwiftUIView/MandelbrotView.swift
+++ MandelbrotExplorer iPad/SwiftUIView/MandelbrotView.swift
@@ -31,27 +31,10 @@ struct MandelbrotView: View {
                     var newLocation = startLocation ?? location
                     newLocation.x += value.translation.width
                     newLocation.y += value.translation.height
-                    
-                    if newLocation.x - 0.5 * length < 0.0 {
-                        newLocation.x = 0.5 * length
-                    }
-                    
-                    if newLocation.x + 0.5 * length > geometry.size.width {
-                        newLocation.x = geometry.size.width - 0.5 * length
-                    }
-                    
-                    if newLocation.y - 0.5 * length < 0.0 {
-                        newLocation.y = 0.5 * length
-                    }
-                    
-                    if newLocation.y + 0.5 * length > geometry.size.height {
-                        newLocation.y = geometry.size.height - 0.5 * length
-                    }
-
-                    location = newLocation
+                    location = validate(location: newLocation, in: geometry.size, given: length)
                 }
                 .onEnded { _ in
-                    viewModel.updateRange(origin: location, length: length, originalLength: bodyLength)
+                    viewModel.needToRefresh.toggle()
                 }
                 
             let magnificationGesture = MagnificationGesture(minimumScaleDelta: 0.001)
@@ -60,30 +43,10 @@ struct MandelbrotView: View {
                 }
                 .onChanged { value in
                     let newLength = length + 5.0 * (value - 1.0)
-                    if newLength < 10.0 {
-                        length = 10.0
-                    } else {
-                        length = newLength
-                    }
-                    
-                    if location.x - 0.5 * length < 0.0 {
-                        length = 2.0 * location.x
-                    }
-                    
-                    if location.x + 0.5 * length > geometry.size.width {
-                        length = 2.0 * (geometry.size.width - location.x)
-                    }
-                    
-                    if location.y - 0.5 * length < 0.0 {
-                        length = 2.0 * location.y
-                    }
-                    
-                    if location.y + 0.5 * length > geometry.size.height {
-                        length = 2.0 * (geometry.size.height - location.y)
-                    }
+                    length = validate(length: newLength, in: geometry.size, given: location)
                 }
                 .onEnded { _ in
-                    viewModel.updateRange(origin: location, length: length, originalLength: bodyLength)
+                    viewModel.needToRefresh.toggle()
                 }
             
             ZStack {
@@ -105,10 +68,10 @@ struct MandelbrotView: View {
                 viewModel.generateMandelbrotSet()
             }
             .onChange(of: geometry.size) { _ in
+                location = validate(location: location, in: geometry.size, given: length)
                 viewModel.needToRefresh.toggle()
             }
             .onChange(of: viewModel.refresh) { _ in
-                let bodyLength = geometry.size.width < geometry.size.height ? geometry.size.width : geometry.size.height
                 viewModel.updateRange(origin: location, length: length, originalLength: bodyLength)
             }
         }
@@ -120,6 +83,53 @@ struct MandelbrotView: View {
             .frame(width: length, height: length)
     }
     
+    private func validate(location: CGPoint, in frame: CGSize, given length: CGFloat) -> CGPoint {
+        var newLocation = location
+        
+        if newLocation.x - 0.5 * length < 0.0 {
+            newLocation.x = 0.5 * length
+        }
+        
+        if newLocation.x + 0.5 * length > frame.width {
+            newLocation.x = frame.width - 0.5 * length
+        }
+        
+        if newLocation.y - 0.5 * length < 0.0 {
+            newLocation.y = 0.5 * length
+        }
+        
+        if newLocation.y + 0.5 * length > frame.height {
+            newLocation.y = frame.height - 0.5 * length
+        }
+        
+        return newLocation
+    }
+    
+    private func validate(length: CGFloat, in frame: CGSize, given location: CGPoint) -> CGFloat {
+        var newLength = length
+        
+        if newLength < 10.0 {
+            newLength = 10.0
+        } else {
+            if location.x - 0.5 * length < 0.0 {
+                newLength = 2.0 * location.x
+            }
+            
+            if location.x + 0.5 * length > frame.width {
+                newLength = 2.0 * (frame.width - location.x)
+            }
+            
+            if location.y - 0.5 * length < 0.0 {
+                newLength = 2.0 * location.y
+            }
+            
+            if location.y + 0.5 * length > frame.height {
+                newLength = 2.0 * (frame.height - location.y)
+            }
+        }
+        
+        return newLength
+    }
     
 }
 
